ea1b0ebc7cb64fbcc62e73eea89e658d97c095e0
cgcl_unravel <vastrockhuang162@gmail.com>
2023-08-20 12:46:13 +0000

    coding: generate_local_dist_size()

--- example/belief_propagation.cc
+++ example/belief_propagation.cc
@@ -38,14 +38,16 @@ void init(int argc, char** argv) {
   google::LogToStderr();
 }
 
+using state_t = uint64_t;
+
 template <typename T>
 struct factor_vertex_t {
-  std::vector<std::pair<plato::vid_t, uint32_t>> vars;
+  std::vector<std::pair<plato::vid_t, state_t>> vars;
   std::vector<T> dists;
 };
 
 template <typename VCACHE>
-std::shared_ptr<VCACHE> load_factor_cache(const std::string& path) {
+std::shared_ptr<VCACHE> load_factors_cache(const std::string& path) {
 
   using dist_t = double;
   using vdata_t = factor_vertex_t<dist_t>;
@@ -62,13 +64,13 @@ std::shared_ptr<VCACHE> load_factor_cache(const std::string& path) {
       char* pSave = nullptr;
       char* pVar = nullptr;
       pVar = strtok_r(pVars, " ", &pSave);
-      uint32_t total_states = 1;
+      state_t total_states = 1;
       while(pVar) {
         char* pDash = std::strchr(pVar, '-');
         if (pDash) {
           *pDash = '\0';
           plato::vid_t vid = std::strtoul(pVar, nullptr, 0);
-          uint32_t state = std::strtoul(pDash + 1, nullptr, 0);
+          state_t state = std::strtoul(pDash + 1, nullptr, 0);
           item->vars.emplace_back(vid, state);
           total_states *= state;
         }
@@ -95,31 +97,39 @@ std::shared_ptr<VCACHE> load_factor_cache(const std::string& path) {
 }
 
 template <typename ECACHE, typename VCACHE>
-std::shared_ptr<ECACHE> load_edges_cache_from_factor_cache(
+std::shared_ptr<ECACHE> load_edges_cache_from_factors_cache(
     VCACHE& pvcache, plato::graph_info_t* pginfo) {
 
   plato::eid_t edges = 0;
   plato::bitmap_t<> v_bitmap(std::numeric_limits<vid_t>::max());
   std::shared_ptr<ECACHE> pecache(new ECACHE());
 
-  plato::traverse_opts_t traverse_opts;
-  pvcache.reset_traversal(traverse_opts);
-  size_t chunk_size = 64;
-  while (pvcache.next_chunk([&](size_t, plato::vertex_unit_t<factor_vertex_t>* v_data)) {
-    edges += v_data->vdata_.vars.size();
-    v_bitmap.set_bit(v_data->vid_);
-    for(const auto& var: v_data->vdata_.vars) {
-      v_bitmap.set_bit(var.first);
-      plato::edge_unit_t<edata_t, vid_t> edge;
-      edge.src_ = v_data->vid_;
-      edge.dst_ = var.first;
-      pecache->push_back(edge);
-    }
-  }) {}
+  pvcache.reset_traversal();
+  #pragma omp parallel
+  {
+    size_t chunk_size = 1;
+    while (pvcache.next_chunk([&](size_t, plato::vertex_unit_t<factor_vertex_t<double>>* v_data) {
+      plato::eid_t var_size = v_data->vdata_.vars.size();
+      v_bitmap.set_bit(v_data->vid_);
+      // use variable vertex to replace factor vertex
+      if (var_size == 1) {
+        v_bitmap.set_bit(v_data->vdata_.vars.front().first);
+        return true;
+      }
+      __sync_fetch_and_add(&edges, var_size);
+      for(const auto& var: v_data->vdata_.vars) {
+        v_bitmap.set_bit(var.first);
+        plato::edge_unit_t<edata_t, plato::vid_t> edge;
+        edge.src_ = v_data->vid_;
+        edge.dst_ = v ar.first;
+        pecache->push_back(edge);
+      }
+      return true;
+    })) {}
+  }
 
-  MPI_Allreduce(MPI_IN_PLACE, &edges, 1, plato::get_mpi_data_type<eid_t>(), MPI_SUM, MPI_COMM_WORLD);
-  plato::allreduce(MPI_IN_PLACE, v_bitmap.data_, plato::word_offset(v_bitmap.size_) + 1, plato::get_mpi_data_type<uint64_t>(),
-      MPI_BOR, MPI_COMM_WORLD);
+  MPI_Allreduce(MPI_IN_PLACE, &edges, 1, plato::get_mpi_data_type<plato::eid_t>(), MPI_SUM, MPI_COMM_WORLD);
+  v_bitmap.sync();
   
   if (pginfo) {
     pginfo->edges_    = edges;
@@ -149,7 +159,7 @@ std::shared_ptr<plato::dcsc_t<plato::empty_t, SEQ_PART>> create_dcsc_seq_from_fa
   watch.mark("t0");
   watch.mark("t1");
 
-  auto cache = load_edges_cache_from_factor_cache<CACHE<edata_t, plato::vid_t>>(
+  auto cache = load_edges_cache_from_factors_cache<CACHE<edata_t, plato::vid_t>>(
     pvcache, pgraph_info);
   
   if (0 == cluster_info.partition_id_) {
@@ -198,6 +208,95 @@ std::shared_ptr<plato::dcsc_t<plato::empty_t, SEQ_PART>> create_dcsc_seq_from_fa
   }
 
   return pdcsc;
+}
+
+enum class bp_vertex_type_t {
+  VARIABLE = 0,
+  FACTOR = 1
+};
+
+struct bp_state_msg_t {
+  plato::vid_t vid;
+  bp_vertex_type_t v_type;
+  state_t dist_size;
+};
+
+template <typename VCACHE, typename PART_IMPL>
+state_t generate_local_dist_size(
+    VCACHE& pvacahe,
+    const plato::graph_info_t& graph_info,
+    std::shared_ptr<PART_IMPL> partitioner) {
+
+  plato::bitmap_t<> visited(graph_info.vertices_);
+
+  auto& cluster_info = cluster_info_t::get_instance();
+
+  state_t local_dist_size = 0;
+
+  plato::bsp_opts_t opts;
+  opts.threads_               = -1;
+  opts.flying_send_per_node_  = 3;
+  opts.flying_recv_           = cluster_info.partitions_;
+  opts.global_size_           = 16 * MBYTES;
+  opts.local_capacity_        = PAGESIZE;
+  opts.batch_size_            = 1;
+
+  pvcache.reset_traversal();
+  auto __send = [&](plato::bsp_send_callback_t<bp_state_msg_t> send) {
+    size_t chunk_size = 1;
+    while (pvcache.next_chunk([&](size_t, plato::vertex_unit_t<factor_vertex_t<double>>* v_data) {
+        plato::eid_t var_size = v_data->vdata_.vars.size();
+        if (var_size != 1) {
+          bp_state_msg_t factor_state;
+          factor_state.vid       = v_data->vid_;
+          factor_state.v_type    = bp_vertex_type_t::FACTOR;
+          factor_state.dist_size = v_data->vdata_.dists.size();
+          send(partitioner->get_partition_id(factor_state.vid), factor_state);
+        }
+        for(const auto& var: v_data->vdata_.vars) {
+          bp_state_msg_t var_state;
+          var_state.vid          = var.first;
+          var_state.v_type       = bp_vertex_type_t::VARIABLE;
+          factor_state.dist_size = var.second;
+          send(partitioner->get_partition_id(var_state.vid), var_state);
+        }
+        return true;
+      }, &chunk_size)) {}
+  };
+
+  auto __recv = [&] (int /* p_i */, plato::bsp_recv_pmsg_t<bp_state_msg_t>& pmsg) {
+    if (visited.get_bit(pmsg->vid) == 0) {
+      visited.set_bit(pmsg->vid);
+      __sync_fetch_and_add(&local_dist_size, pmsg->dist_size);
+    }
+  };
+
+  auto rc = fine_grain_bsp<bp_state_msg_t>(__send, __recv, opts);
+  if (0 != rc) {
+    LOG(ERROR) << "bsp failed with code: " << rc;
+    return -1;
+  }
+
+  return local_dist_size;
+}
+
+template <typename VCACHE, typename GRAPH>
+void init_states_from_factors_cache(
+    VCACHE& pvcache,
+    GRAPH& graph,
+    const plato::graph_info_t& graph_info) {
+    
+  auto partitioner = graph.partitioner();
+
+  state_t local_dist_size = generate_local_dist_size(pvcache, graph_info, partitioner);
+  
+  // TODO:
+  // 1. compute total out_degree
+  // 2. alloc factors' var_states arr,
+  // 3. alloc all vertices' dist arr and bp arr
+  // 4. init all vertices' states -- dist
+  // 5. alloc all vertices' msgs_ptrs arr and msgs arr
+
 }
 
 int main(int argc, char** argv) {
